3ca8eabeb0aa339a51e19879859487261566e48f
Pritham Baswani Girylkar <108477555+spiman9@users.noreply.github.com>
2023-10-23 15:10:00 +0530

    aded the backed development

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
     <div name="home" className='h-screen w-full bg-gradient-to-b from-black via-black to-gray-800 pt-24 md:pt-0'>
         <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row'>
             <div className='flex flex-col justify-center h-full'>
-                <h2 className='text-4xl  sm:text-7xl font-bold text-white'>I'm a Full Stack Developer</h2>
+                <h2 className='text-4xl  sm:text-7xl font-bold text-white'>I'm a Back Stack Developer</h2>
                 <p className='text-gray-500 py-4 max-w-md'>
                     Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore dolorum culpa tenetur voluptates velit, ut in cumque molestiae. Accusantium optio ea itaque esse quod adipisci!
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius iusto nihil porro quam harum. Veniam.
