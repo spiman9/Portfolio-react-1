c6cbbd61bada086e8c8074a672fab60bce0d09f3
Pritham Baswani Girylkar <108477555+spiman9@users.noreply.github.com>
2023-10-24 02:18:55 +0530

    last night commits

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -32,7 +32,7 @@ const NavBar = () => {
   return (
     <div className='flex justify-between items-center w-full h-20 px-4 text-white fixed bg-black'>
         <div>
-            <h1 className='text-5xl font-signature ml-2'><Link to="home" smooth duration={500}>Pritham</Link></h1>
+            <h1 className='text-5xl font-signature ml-2'><Link to="home" smooth duration={500}>Pritham B G</Link></h1>
         </div>
 
         <ul className='hidden md:flex'>
